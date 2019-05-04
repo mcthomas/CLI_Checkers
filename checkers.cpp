83da98e93684deca12bc776c2d74a6b411e2f62e
mcthomas <mcthomas@users.noreply.github.com>
2019-05-04 17:51:28 -0500

    Created checker.cpp

---
+++ checkers.cpp
@@ -0,0 +1,26 @@
+/* ******************************************************
+ * CS368 FINAL PROJECT: CLI_Checkers
+ * Main File: checkers.cpp
+ * Names: Matt Thomas, Aleks Lesiewicz, and Parker Breene
+ *******************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Designates current state of the game
+class Board {
+    
+}
+
+//Simple AI; randomly executes valid moves in turn
+class AI {
+    
+}
+
+//Handles gameplay flow / user input
+int main(int argc, char**argv)
+{
+    return 0;
+}
