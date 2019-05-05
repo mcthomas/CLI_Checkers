abb21f0a59fdff13c04464e6c3c2a68d656e004e
mcthomas <mcthomas@users.noreply.github.com>
2019-05-04 21:05:30 -0500

    + Board globals / method stubs

--- checkers.cpp
+++ checkers.cpp
@@ -6,20 +6,41 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 //Designates current state of the game
 class Board {
     
-}
+    char black = '⬓';
+    char red = '⬕';
+    char blackKing = '⬒'
+    char redKing = '⬔
+    char space = '⧠'
+    vector< vector<char> > board;
+    board.resize(8, std::vector<int>(8, '⧠'));
+    
+    bool checkValid(int x, int y) {}
+    
+    bool checkOpen(int x, int y) {}
+    
+    void move(int x, int y, char symbol) {}
+    
+    bool checkEnd() {}
+    
+    bool checkTie() {}
+    
+    void updateBoard() {}
+    
+};
 
 //Simple AI; randomly executes valid moves in turn
 class AI {
     
-}
+};
 
-//Handles gameplay flow / user input
+//Handles gameplay loop / user input
 int main(int argc, char**argv)
 {
     return 0;
